a293f97ba16fa9f3519c99fca52fe772cceceae9
Andrey Kirichenko <andrey.kirichenko999@gmail.com>
2019-04-28 17:34:21 +0300

    error boundary

--- src/components/error-indicator/error-indicator.js
+++ src/components/error-indicator/error-indicator.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './error-indicator.css';
 
 const ErrorIndicator = () => {
-  return <div>Error!!!</div>;
+  return (
+    <div>
+      Ooops! Error!
+    </div>
+  );
 };
 
 export default ErrorIndicator;
