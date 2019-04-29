0a3f5ff1d2a1e989e6196f172bbca23e89be05cf
Andrey Kirichenko <andrey.kirichenko999@gmail.com>
2019-04-29 12:46:58 +0300

    added shop-header

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import ShopHeader from '../shop-header';
 import { HomePage, CartPage } from '../pages';
 
 import './app.css';
@@ -9,6 +10,7 @@ const App = () => {
 
   return (
     <main role='main' className='container'>
+      <ShopHeader numItems={5} total={210} />
       <Switch>
         <Route path='/'
                component={HomePage}
